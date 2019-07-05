6fa013dc53ee5534590c610a7a826c3b5211060e
yht2004 <1921959159@qq.com>
2019-07-05 10:55:08 +0800

    测试

--- src/main/java/com/yanghongtao/springboot/controller/LoginController.java
+++ src/main/java/com/yanghongtao/springboot/controller/LoginController.java
@@ -2,18 +2,21 @@ package com.yanghongtao.springboot.controller;
 
 import com.yanghongtao.springboot.entities.User;
 import com.yanghongtao.springboot.mapper.UserMapper;
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
 public class LoginController {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     UserMapper userMapper;
@@ -47,6 +50,26 @@ public class LoginController {
     @RequestMapping("/user/pwd")
     public String getUpdatePwdPage(){
         return "main/password";
+    }
+
+    @ResponseBody
+    @GetMapping("/user/pwd/{oldPwd}")
+    public Boolean checkPwd(HttpSession session,@PathVariable("oldPwd") String oldPwd){
+        logger.info("原密码:" + oldPwd);
+        User user = (User)session.getAttribute("loginUser");
+        if(user.getPassword().equals(oldPwd)){
+            return true;
+        }
+        return false;
+    }
+
+    @PostMapping("/user/pwd")
+    public String updatePwd(HttpSession session, String password){
+        logger.info("新密码：" + password);
+        User user = (User)session.getAttribute("loginUser");
+        user.setPassword(password);
+        userMapper.updateUser(user);
+        return "redirect:/logout";
     }
 
 
